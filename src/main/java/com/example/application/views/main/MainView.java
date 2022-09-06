4002f3c311d755da0e6b3147a58217d97c3f6799
sergei <alalon211090@gmail.com>
2022-09-06 20:26:06 +0300

    push

--- src/main/java/com/example/application/views/main/MainView.java
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -10,36 +10,10 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
-@PageTitle("Main")
+@PageTitle("Contacts | Vaadin CRM")
 @Route(value = "")
 public class MainView extends VerticalLayout {
 
-//    private TextField name;
-//    private Button sayHello;
-
     public MainView() {
-        Button button = new Button ("Click Me");
-        TextField name = new TextField("Name");
-       // add(name, button);
-
-        HorizontalLayout h1 = new HorizontalLayout(name, button);
-        h1.setDefaultVerticalComponentAlignment(Alignment.BASELINE);
-
-        button.addClickListener(click-> Notification.show("Hello " + name.getValue()));
-        add(h1);
-
-
-  /*      name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
-        });
-        sayHello.addClickShortcut(Key.ENTER);
-
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
-
-       add(name, sayHello);
-       */
     }
 }
