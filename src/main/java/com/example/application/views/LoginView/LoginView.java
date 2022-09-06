ed006a565af05eaca2c79447fcbb13b4033d5fbf
jarvis <dmitrii.zolotenkov@diginetica.com>
2022-09-06 20:54:01 +0300

    try

--- src/main/java/com/example/application/views/LoginView/LoginView.java
+++ src/main/java/com/example/application/views/LoginView/LoginView.java
@@ -1,4 +1,5 @@
 package com.example.application.views.LoginView;
+
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.login.LoginForm;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
@@ -8,31 +9,30 @@ import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 @Route("login")
-@PageTitle("Login | Vaadin CRM")
+@PageTitle("Login")
 public class LoginView extends VerticalLayout implements BeforeEnterObserver {
 
+  private LoginForm login = new LoginForm();
 
-        private final LoginForm login = new LoginForm();
+  public LoginView() {
+    addClassName("login-view");
+    setSizeFull();
 
-        public LoginView(){
-            addClassName("login-view");
-            setSizeFull();
-            setAlignItems(Alignment.CENTER);
-            setJustifyContentMode(JustifyContentMode.CENTER);
+    setJustifyContentMode(JustifyContentMode.CENTER);
+    setAlignItems(Alignment.CENTER);
 
-            login.setAction("login");
+    login.setAction("login");
 
-            add(new H1("Vaadin CRM"), login);
-        }
+    add(new H1("Test Application"), login);
+  }
 
-        @Override
-        public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
-            // inform the user about an authentication error
-            if(beforeEnterEvent.getLocation()
-                    .getQueryParameters()
-                    .getParameters()
-                    .containsKey("error")) {
-                login.setError(true);
-            }
-        }
+  @Override
+  public void beforeEnter(BeforeEnterEvent beforeEnterEvent) {
+    if(beforeEnterEvent.getLocation()
+      .getQueryParameters()
+      .getParameters()
+      .containsKey("error")) {
+      login.setError(true);
     }
+  }
+}
