b2fd32a063ba9cf46566eeb8061978222e5a4b56
sergei <alalon211090@gmail.com>
2022-09-13 20:14:54 +0300

    CompanyRepository

---
+++ src/main/java/com/example/application/data/entity/CompanyRepository.java
@@ -0,0 +1,7 @@
+package com.example.application.data.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CompanyRepository extends JpaRepository<Company, Integer> {
+}
+
