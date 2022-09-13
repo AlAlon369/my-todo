29495a735def8a408acc162bb6f41fb229d1be25
sergei <alalon211090@gmail.com>
2022-09-13 20:16:25 +0300

    ContactRepository

--- src/main/java/com/example/application/data/entity/ContactRepository.java
+++ src/main/java/com/example/application/data/entity/ContactRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-
+//
 public interface ContactRepository extends JpaRepository<Contact, Integer> {
 
     @Query("select c from Contact c " +
