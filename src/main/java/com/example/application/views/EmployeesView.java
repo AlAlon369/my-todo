1b4f90227dfbd852201ee9e334fe5f8c17b72dd6
sergei <alalon211090@gmail.com>
2022-09-17 13:54:31 +0300

    Добавил кнопку которая ведет на страницу "add-employees"

--- src/main/java/com/example/application/views/EmployeesView.java
+++ src/main/java/com/example/application/views/EmployeesView.java
@@ -7,16 +7,20 @@ import javax.annotation.security.RolesAllowed;
 
 import com.example.application.data.entity.Employee;
 import com.example.application.data.repository.EmployeeRepository;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
 
 @Route(value = "employees", layout = AppLayoutBasic.class)
 @RolesAllowed("USER")
 public class EmployeesView extends FormLayout {
 
-    public EmployeesView(EmployeeRepository employeeRepository) {
 
+
+    public EmployeesView(EmployeeRepository employeeRepository) {
         Grid<Employee> grid = new Grid<>(Employee.class, false);
         grid.addColumn(Employee::getFirstName).setHeader("First name");
         grid.addColumn(Employee::getLastName).setHeader("Last name");
@@ -26,7 +30,11 @@ public class EmployeesView extends FormLayout {
         List<Employee> all = employeeRepository.findAll();
         grid.setItems(all);
 
-        add(grid);
+        Button button = new Button("Добавить сотрудника");
+        button.addClickListener(e ->
+                button.getUI().ifPresent(ui ->
+                        ui.navigate("add-employees"))
+        );
     }
 }
 
