48fcfef45dea5a90492086db2539f7edb1d604d1
jarvis <dmitrii.zolotenkov@diginetica.com>
2022-09-22 12:20:19 +0300

    Настройка колонки "Нанят"

--- src/main/java/com/example/application/views/EmployeesView.java
+++ src/main/java/com/example/application/views/EmployeesView.java
@@ -8,7 +8,9 @@ import org.vaadin.crudui.form.CrudFormFactory;
 
 import com.example.application.data.entity.Employee;
 import com.example.application.data.repository.EmployeeRepository;
+import com.vaadin.flow.component.checkbox.Checkbox;
 import com.vaadin.flow.component.formlayout.FormLayout;
+import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
@@ -17,33 +19,37 @@ import com.vaadin.flow.router.Route;
 public class EmployeesView extends FormLayout {
 
     public EmployeesView(EmployeeRepository repository) {
-
         GridCrud<Employee> crud = new GridCrud<>(Employee.class);
         crud.setFindAllOperation(repository::findAll);
         crud.setAddOperation(repository::save);
         crud.setUpdateOperation(repository::save);
         crud.setDeleteOperation(repository::delete);
 
-        translateColumns(crud);
+        setColumns(crud);
         translateForms(crud);
 
         add(crud);
     }
 
-    private  void translateForms(GridCrud<Employee> crud) {
+    private void translateForms(GridCrud<Employee> crud) {
         CrudFormFactory<Employee> crudFormFactory = crud.getCrudFormFactory();
-        crudFormFactory.setVisibleProperties("firstName", "lastName", "middleName", "phone");
+        crudFormFactory.setVisibleProperties("firstName", "lastName", "middleName", "phone", "hired");
         crudFormFactory.setFieldCreationListener("firstName", field -> ((TextField) field).setLabel("Имя"));
         crudFormFactory.setFieldCreationListener("lastName", field -> ((TextField) field).setLabel("Фамилия"));
         crudFormFactory.setFieldCreationListener("middleName", field -> ((TextField) field).setLabel("Отчество"));
         crudFormFactory.setFieldCreationListener("phone", field -> ((TextField) field).setLabel("Телефон"));
+        crudFormFactory.setFieldCreationListener("hired", field -> ((Checkbox) field).setLabel("Нанят"));
     }
 
-    private void translateColumns(GridCrud<Employee> crud) {
-        crud.getGrid().getColumnByKey("firstName").setHeader("Имя");
-        crud.getGrid().getColumnByKey("lastName").setHeader("Фамилия");
-        crud.getGrid().getColumnByKey("middleName").setHeader("Отчество");
-        crud.getGrid().getColumnByKey("phone").setHeader("Телефон");
+    private void setColumns(GridCrud<Employee> crud) {
+        Grid<Employee> grid = crud.getGrid();
+        grid.getColumnByKey("firstName").setHeader("Имя");
+        grid.getColumnByKey("lastName").setHeader("Фамилия");
+        grid.getColumnByKey("middleName").setHeader("Отчество");
+        grid.getColumnByKey("phone").setHeader("Телефон");
+        grid.getColumnByKey("hired").setVisible(false);
+        grid.getColumnByKey("id").setVisible(false);
+        grid.addColumn(i -> i.isHired() ? "Да" : "Нет").setHeader("Нанят");
     }
 }
 
