363252d6608f3f877320fb55e452d59dde3727fc
jarvis <dmitrii.zolotenkov@diginetica.com>
2022-10-08 00:21:42 +0600

    Добавил комбобокс сотрудника

--- src/main/java/com/example/application/views/TimeSheetView.java
+++ src/main/java/com/example/application/views/TimeSheetView.java
@@ -2,25 +2,53 @@ package com.example.application.views;
 
 import com.example.application.controller.TimeSheetController;
 import com.example.application.data.TimeSheetDto;
+import com.example.application.data.entity.Employee;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.grid.Grid;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+
 import org.vaadin.crudui.crud.impl.GridCrud;
 import org.vaadin.crudui.layout.impl.VerticalCrudLayout;
 
 import javax.annotation.security.RolesAllowed;
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Route(value = "timesheet", layout = AppLayoutBasic.class)
 @RolesAllowed("USER")
 public class TimeSheetView extends FormLayout {
   public TimeSheetView(TimeSheetController controller) {
+    ComboBox<Employee> employeeComboBox = createEmployeeComboBox(controller);
+    GridCrud<TimeSheetDto> crud = createTimeSheetGrid(controller);
+    VerticalLayout layout = new VerticalLayout(employeeComboBox, crud);
+    add(layout);
+  }
+
+  private ComboBox<Employee> createEmployeeComboBox(TimeSheetController controller) {
+    ComboBox<Employee> comboBox = new ComboBox<>("Сотрудник");
+    comboBox.getStyle().set("--vaadin-combo-box-overlay-width", "350px");
+    ComboBox.ItemFilter<Employee> filter = (person, filterString) ->
+      (person.getLastName() + " " + person.getFirstName()).toLowerCase().contains(filterString.toLowerCase());
+    List<Employee> employees = controller.findAll()
+      .stream()
+      .map(TimeSheetDto::getEmployee)
+      .collect(Collectors.toList());
+    comboBox.setItems(filter, employees);
+    comboBox.setItemLabelGenerator(person -> person.getLastName() + " " + person.getFirstName());
+    return comboBox;
+  }
+
+  private GridCrud<TimeSheetDto> createTimeSheetGrid(TimeSheetController controller) {
     GridCrud<TimeSheetDto> crud = new GridCrud<>(TimeSheetDto.class, new VerticalCrudLayout());
     setButtonsInvisible(crud);
     crud.setFindAllOperation(controller::findAll);
     setColumns(crud);
-    add(crud);
+    return crud;
   }
 
   private void setButtonsInvisible(GridCrud<TimeSheetDto> crud) {
